5ed57da5574a006ed66f623cfa73884e85a93659
TheScarletBadger <barry.crouch@outlook.com>
2026-02-22 23:31:13 +0000

    First update

--- FE_Train_Model_v2.py
+++ FE_Train_Model_v2.py
@@ -3,6 +3,8 @@
 Antonis Michalas. (2019).
 Text files from Gutenberg database [Data set].
 Zenodo. https://doi.org/10.5281/zenodo.3360392
+
+https://archive.org/stream/finneganswake00joycuoft/finneganswake00joycuoft_djvu.txt
 """
 #from gensim.models import Word2Vec
 import nltk
@@ -10,36 +12,36 @@ import re
 from os import listdir
 import gc
 from gensim.models import Word2Vec
+from multiprocessing import cpu_count
 
+cores = cpu_count()
+nltk.download('punkt_tab')
 cnt = 1
-datadir = 'D670MB+FW/'
+datadir = 'D184MB+FW/'
 corpus = ''
 for filename in listdir(datadir):
     print(f'Processing file {cnt}: {filename}')
+    f = open(datadir + filename ,'r')
     try:
-        f = open(datadir + filename ,'r')
         corpus = corpus + f.read()
     except:
-        print('Error reading file {filename} Skipping')
+        print(f'Error reading file {filename} Skipping')
     f.close()
     cnt=cnt+1
 
-print('Cleaning and de-duplicating tokens...')    
+print('Cleaning tokens...')    
 #remove newline characters
 corpus = corpus.replace('\n', ' ').lower()
 
 #remove characters that are not letters, spaces or periods
-cleaned = re.sub(r"[^A-Za-z\s.]", "", corpus)
+cleaned = re.sub(r"[^A-Za-z\s.'-]", "", corpus)
 del(corpus)
 gc.collect()
 
 #split corpus into list sentences
-nltk.download('punkt_tab')
 sentences = nltk.sent_tokenize(cleaned)
 del(cleaned)
 gc.collect()
-
-sentences=list(dict.fromkeys(sentences))
 
 #split each item in sentence list into list of words
 words = [nltk.tokenize.word_tokenize(i) for i in sentences]
@@ -48,9 +50,10 @@ gc.collect()
 
 print('Training model...')    
 #initialize and train model
-model = Word2Vec(vector_size=1024, window=10, workers=4, min_count=1, seed=42)
+model = Word2Vec(vector_size=500, window=10, workers=cores, min_count=1, seed=42)
 model.build_vocab(words)
-model.train(words, total_examples=len(words), epochs=100)
+model.train(words, total_examples=len(words), epochs=25)
+model.save('finnegan.model')
 
 #print closest words to man and woman in embedding space
 print(model.wv.most_similar('man'))
