ceabc8bece74ed63e5d27f3bb61fbe169cb090e1
TheScarletBadger <56405836+TheScarletBadger@users.noreply.github.com>
2026-02-24 00:28:25 +0000

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 import enchant
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
-from gensim.models import Word2Vec
+from gensim.models import KeyedVectors
 import pandas as pd
 from numpy import sqrt
 
@@ -18,7 +18,7 @@ gr.close_all()
 d = enchant.Dict("en_GB")
 
 #load trained model from disk
-model = Word2Vec.load('finnegan.model')
+model = KeyedVectors.load('finnegan_keyed_vectors.kv')
 
 #load list of finneganisms
 with open('finneganisms.txt') as file:
@@ -26,16 +26,19 @@ with open('finneganisms.txt') as file:
 
 #fit scaler and perform pca over scaled embeddings
 scaler = StandardScaler()
-scaler.fit(model.wv.vectors)
+scaler.fit(model.vectors)
 pca = PCA(n_components=2)
-pca.fit(scaler.transform(model.wv.vectors))
+pca.fit(scaler.transform(model.vectors))
 
 def findnearest(targetword):
+    targetword = targetword.lower()
     targloc = pca.transform(scaler.transform(model.wv[targetword].reshape(1, -1)))
-    targdf = pd.DataFrame(model.wv.most_similar(targetword,topn=1000),columns=['label','sim'])
+    targdf = pd.DataFrame(model.most_similar(targetword,topn=10000),columns=['label','sim'])
     targdf['english'] = targdf['label'].apply(d.check)
+    targdf['finneganism'] = targdf['label'].isin(finneganisms)
     targdf[["pc1","pc2"]] = pca.transform(scaler.transform(model.wv[targdf['label']]))
     targdf["targdist"] = sqrt((targdf['pc1'] - targloc[0][0])**2 + (targdf['pc2'] - targloc[0][1])**2)
+    targdf = targdf[targdf['english'] | targdf['finneganism']]
     return targdf
 
 
